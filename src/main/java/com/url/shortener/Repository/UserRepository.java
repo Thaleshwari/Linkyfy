d6ceccceea3a437e0f4fd2a621c64c14dd9b98d2
Thaleshwari <thaleshwari2005@gmail.com>
2026-03-07 20:44:22 +0530

    JWT implementation completed

---
+++ src/main/java/com/url/shortener/Repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.url.shortener.Repository;
+
+import com.url.shortener.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Override
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/url/shortener/Security/JwtAuthenticationResponse.java
@@ -0,0 +1,11 @@
+package com.url.shortener.Security;
+
+
+import lombok.Data;
+
+@Data
+public class JwtAuthenticationResponse {
+    private String token;
+
+
+}
